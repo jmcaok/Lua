d7c07a678059e7232f2c941a87064d61b6b0c1da
jmcaok <jmchasson@gmail.com>
2024-11-10 12:42:45 +0200

    added functionality
    - tells me the thing the bot did
    - the rest of the ways to win
    - a randomizer

--- rps/rps.lua
+++ rps/rps.lua
@@ -1,13 +1,21 @@
-io.write("enter rock, paper, or scissors\n")
+while true do
+	io.write("enter rock, paper, or scissors\n")
 
-rpsp = io.read()
+	rpsp = io.read()
+	if rpsp == "-1" then
+		break
+	end
+	rpsc = { "rock", "paper", "scissors" }
 
-rpsc = "rock"
-
-if rpsp == "paper" and rpsc == "rock" then
-    io.write("player: I win\n")
-elseif rpsp == "rock" and rpsc == "rock" then
-    io.write("player and computer: tie\n")
-elseif rpsp == "scissors" and rpsc == "rock" then
-    io.write("computer: you lost\n") 
+	ran = math.random(3)
+	io.write("computer move index:\t", ran, "\n")
+	continuemove = rpsc[ran]
+	io.write("computer move:\t", continuemove, "\n")
+	if rpsp == "paper" and continuemove == "rock" or rpsp == "scissors" and continuemove == "paper" or rpsp == "rock" and continuemove == "scissors" then
+		io.write("player: I win\n")
+	elseif rpsp == continuemove then
+		io.write("player and computer: tie\n")
+	else
+		io.write("computer: you lost\n")
+	end
 end
